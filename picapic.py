b9e6820ce4205827d85edd111f327561d5902242
seth <seth.newbill@gmail.com>
2025-02-02 22:25:37 -0500

    '2025-02-02 commit'

--- picapic.py
+++ picapic.py
@@ -1,9 +1,11 @@
 from picamzero import Camera
 from datetime import date
+import git
 import os
 
+today = date.today()
 path = "/home/styrofomeBoots/Desktop/picapic/pics"
-image_path = f"{path}/{date.today()}.jpg"
+image_path = f"{path}/{today}.jpg"
 
 if not os.path.exists(path):
 	os.makedirs(path)
@@ -12,3 +14,9 @@ cam = Camera()
 cam.start_preview()
 cam.take_photo(image_path)
 cam.stop_preview()
+
+r = git.Repo()
+r.git.pull()
+r.git.add(u=True)
+r.git.commit(f"-m'{today} commit'")
+r.git.push()
