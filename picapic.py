cf542b053c3e579979c1333e43412ed5bccc7bce
seth <seth.newbill@gmail.com>
2025-02-02 23:32:28 -0500

    2025-02-02 commit

--- picapic.py
+++ picapic.py
@@ -1,22 +1,39 @@
 from picamzero import Camera
-from datetime import date
+from datetime import date, datetime
 import git
+import sys
 import os
 
+def throw_error(e):
+	error_time = datetime.now().strftime("%Y-%m-%d %H: %M: %S")
+	print(f"[{error_time}] ${e}")
+	sys.exit(1)
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+os.chdir(SCRIPT_DIR)
+
 today = date.today()
-path = "/home/styrofomeBoots/Desktop/picapic/pics"
-image_path = f"{path}/{today}.jpg"
+image_dir = os.path.join(SCRIPT_DIR, "pics")
+image_path = os.path.join(image_dir, f"{today}.jpg")
 
-if not os.path.exists(path):
-	os.makedirs(path)
+try:
+	os.makedirs(image_dir, exist_ok=True)
+except Exception as e:
+	throw_error(f"Directory Error: {e}")
 
-cam = Camera()
-cam.start_preview()
-cam.take_photo(image_path)
-cam.stop_preview()
+try:
+	cam = Camera()
+	cam.start_preview()
+	cam.take_photo(image_path)
+	cam.stop_preview()
+except Exception as e:
+	throw_error(f"Image error: {e}")
 
-r = git.Repo()
-r.git.pull()
-r.git.add(A=True)
-r.git.commit("-m", f"{today} commit")
-r.git.push()
+try:
+	r = git.Repo(SCRIPT_DIR)
+	r.git.pull()
+	r.git.add(A=True)
+	r.git.commit("-m", f"{today} commit")
+	r.git.push()
+except Exception as e:
+	throw_error(f"Git error: {e}")
